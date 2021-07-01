bf1e7870f1d339222509865b82779251940191c4
Federico Giolli <fgiolli003@gmail.com>
2021-07-01 20:17:04 -0300

    first commit

---
+++ scrap.py
@@ -0,0 +1,168 @@
+from logging import exception
+import requests
+from bs4 import BeautifulSoup
+import json
+import math
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import selenium.common.exceptions as EX
+# DOCUMENT 
+# F AS FUNCTION 
+# P AS PARAMETER 
+
+#f(p): this saves the actual driver.source of your selenium driver as web.html, param = driver, options: write
+def saveWebFile(driver):
+    file = open('web.html', 'w')
+    file.write(driver.page_source)
+    file.close()
+    
+#f(): opens a file named "web.html" options: readonly
+def getWebFile():
+    return open('web.html','r')
+
+#f(): opens a file named "movies.json" options: write
+def saveJson(data):
+    f = open('movies.json', 'w')    
+    f.write(json.dumps(data))
+    f.close
+
+#f(): retrieve a list of category(movies) links
+def getWebMenuItems():
+    url = "https://www.looke.com.br/"
+    for intentos in range(10):
+        try:
+            webpage = requests.get(url)
+            res = webpage.content
+            webpage = BeautifulSoup(res, "lxml")
+            menu = webpage.find_all("div", {"class":"menuItem"})
+            menu_links = []
+            for menu_category in menu:
+                menu_pages = menu_category.find_all("a", {"class":"headerMenuItenDescription"})
+                for menu_page in menu_pages:
+                    menu_links.append(menu_page["href"].strip())
+            return menu_links
+        except requests.exceptions.ConnectionError:
+            print("Couldnt Connect Attempts: ")
+            continue
+        else:
+            break
+
+#f(p): this saves the actual driver.source, param = urltosave
+def saveSourceFromSelenium(url):
+    options = webdriver.FirefoxOptions()
+    options.add_argument('-headless')
+    driver = webdriver.Firefox(options=options)
+    for intentos in range(10):
+        try:
+            driver.get(url)
+            wait = WebDriverWait(driver, 15) #more for slow network connections
+            loading = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "media-image")))
+            Y = driver.execute_script('return scrollY')
+            max_Y = driver.execute_script('return scrollMaxY')
+            while Y != max_Y:
+                driver.execute_script('scrollTo(0, scrollMaxY)')
+                Y = math.ceil(float(driver.execute_script('return scrollY')))
+                time.sleep(1.5)
+                max_Y = driver.execute_script('return scrollMaxY')
+            time.sleep(10)
+            saveWebFile(driver)
+            driver.close()
+        except EX.TimeoutException:
+            print("Timeout, retrying ")
+            continue
+        else:
+            break
+    
+#f(): scrap all urls from the movies listed in a file.html
+def scrapUrlMovies():
+    file = getWebFile()
+    sopa = BeautifulSoup(file, 'lxml')
+    lista_peliculas = sopa.find_all("div", {"class":"mediaContainer"})
+    urls = []
+    for peliculas in lista_peliculas:
+        pelicula_url = peliculas.find("video")
+        pelicula_url = "https://www.looke.com.br/" + pelicula_url["onclick"][16:].replace("')", "")
+        urls.append(pelicula_url)
+    return urls
+
+#f(p): returns all data movie from an url, params = urlmovie
+def scrapDataMovies(url):
+    for intentos in range(10):
+        try:
+            res = requests.get(url)
+            webpage = res.content
+            sopa = BeautifulSoup(webpage, "lxml").find_all("div", {"class": "detailsMedia"})
+            for info_movie in sopa:
+                movie = {}
+                cast = []
+                crew = []
+                nro_capitulos = 0
+                title = info_movie.find("div", {"class": "detailTitle"}).get_text().split(' - ')
+                if title:
+                    movie['Title'] = title.pop(0)
+                if title:
+                    movie['Temporada'] = title.pop()
+
+                movie['URL'] = url
+                movie['Ano'] = info_movie.find("div", {"class": "detailsYear"}).get_text().split(' | ')[0].strip()
+                movie['Pais'] = info_movie.find("div", {"class": "detailsYear"}).get_text().split(' | ')[1].strip()
+                movie['Duracion'] = info_movie.find("div", {"class": "detailsYear"}).get_text().split(' | ')[2].strip()
+                if info_movie.find_all_next("div", {"class": "movieActorsContainer"})[1]:
+                    for directive in info_movie.find_all_next("div", {"class": "movieActorsContainer"})[1].find_all("a"):
+                        crew.append(directive.get_text())
+                if info_movie.find_all_next("div", {"class": "movieActorsContainer"})[0].find_all("a"):
+                    for actor in info_movie.find_all_next("div", {"class": "movieActorsContainer"})[0].find_all("a"):
+                        cast.append(actor.get_text())
+                        if str(actor.get_text()) != "Não Disponível":
+                            crew.append(actor.get_text())
+
+                movie['Cast'] = cast
+                if cast:
+                    if cast[0] == "Não Disponível":
+                        movie['Cast'] = ",".join(cast)
+                movie['Crew'] = crew
+                movie['Generos'] = info_movie.find(
+                    "div", {"class": "detailsGenre"}).get_text().split(', ')
+
+                for capitulos in info_movie.find_all_next("div", {"class": "episodeName"}):
+                    nro_capitulos += 1
+                movie['Episodios'] = nro_capitulos
+                return movie
+        except requests.exceptions.ConnectionError:
+            print("Couldnt Connect")
+            continue
+        else:
+            break
+
+if __name__ == "__main__":
+    movies = []
+    urls = []
+    ROOT = "https://www.looke.com.br"
+    CATEGORIAS_LINK = getWebMenuItems()
+    for link in CATEGORIAS_LINK:
+        for intentos in range(10):
+            try:
+                print("gettin content")
+                saveSourceFromSelenium(ROOT + link)
+                urls = scrapUrlMovies()
+                for url in urls:
+                    print("scrapping from.. " + url)
+                    movies.append(scrapDataMovies(url))
+                    print("status.. ok")
+                print("saving contnt")
+                saveJson(movies)
+                print("done")
+            except ConnectionError:
+                print("fail gettin content, attempt ")
+                continue
+            else:
+                break
+        else:
+            print("We couldn't get all data from the given url, check your network")
+    print("deleting duplicates..")
+    unique = { each['URL'] : each for each in movies }.values()
+    saveJson(unique)
+    print("done")
